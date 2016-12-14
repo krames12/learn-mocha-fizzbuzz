6089232b87e1848e955913fd6c887f52625c2210
Nick Kramer <krames12@gmail.com>
2016-12-14 11:00:02 -0500

    simple html and css for GUI and comment headings in js file added

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,24 @@
+// meat and potatoes of fizzbuzz itself
 function fizzBuzz(num) {
-    if(num % 3 == 0 && num % 5 == 0) {
-      return 'fizzbuzz';
-    }
-    else if(num % 3 == 0) {
-      return 'fizz';
-    }
-    else if(num % 5 == 0) {
-      return 'buzz';
-    }
-    else {
-      return num.toString();
-    }
+  if(num % 3 == 0 && num % 5 == 0) {
+    return 'fizzbuzz';
+  }
+  else if(num % 3 == 0) {
+    return 'fizz';
+  }
+  else if(num % 5 == 0) {
+    return 'buzz';
+  }
+  else {
+    return num.toString();
+  }
 }
 
+// handling DOM elements for single responsibility purposes
+
+// event listeners
+
+// exporting the fizzBuzz function for mocha tests
 module.exports = {
   fizzBuzz
 };
