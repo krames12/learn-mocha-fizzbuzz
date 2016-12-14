f97a35457ad34c411545f77b8277bcc7172985d0
Nick Kramer <krames12@gmail.com>
2016-12-14 19:12:18 +0000

    dynamic number checking works. functionally done.
    I learned that there is an  event that fires when entering anything into an input field. Might make it look a little prettier but for now it does what it needs to do the way I want it.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,12 +1,12 @@
 // meat and potatoes of fizzbuzz itself
 function fizzBuzz(num) {
-  if(num % 3 == 0 && num % 5 == 0) {
+  if(num % 3 == 0 && num % 5 == 0 && num != 0) {
     return 'fizzbuzz';
   }
-  else if(num % 3 == 0) {
+  else if(num % 3 == 0 && num != 0) {
     return 'fizz';
   }
-  else if(num % 5 == 0) {
+  else if(num % 5 == 0 && num != 0) {
     return 'buzz';
   }
   else {
@@ -16,16 +16,23 @@ function fizzBuzz(num) {
 
 // handling DOM elements for single responsibility purposes
 function domResult(event) {
-  event.preventDefault();
+	console.log(event);
+  // event.preventDefault();
   var num = document.querySelector('#number-input');
   var domOutput = document.querySelector('#result');
 
   domOutput.innerHTML = fizzBuzz(num.value);
 }
 
+// listening for input event on #number-input
+var numInput = document.getElementById('number-input');
+numInput.addEventListener('input', domResult);
+
 // event listeners
 var form = document.getElementById("fizz-form");
-form.addEventListener('submit', domResult);
+form.addEventListener('submit', function(event) {
+	event.preventDefault();
+});
 
 // exporting the fizzBuzz function for mocha tests
 module.exports = {
